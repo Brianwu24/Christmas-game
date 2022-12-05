07c9d3b72e19aeec8eec311b6aa1371044e2166b
Brianwu24 <113204111+Brianwu24@users.noreply.github.com>
2022-12-05 15:43:52 -0500

    Update image_compare.py

--- image_compare.py
+++ image_compare.py
@@ -1,12 +1,16 @@
-import numpy as np
-
 #comapre the images, and check if they are similar, if they are similar then return True, else if they are different return False
-# return True if the image is above 21% in similarity
+# return True if the image is above 31% in deviation
 def compare_images(input_image, input_image_list): #checks if the image passes the test
     image = input_image.copy()
     for reference_image in input_image_list.copy():
-        deviation = np.mean(image != reference_image)
-        if deviation <= 0.21: #below 21% mean that they are similar
+        deviation = 0
+        for y, pixel_row in enumerate(reference_image): # for every pixel check if they are similar
+            for x, pixel in enumerate(pixel_row):
+                if image[y][x] != pixel:
+                        deviation += 1
+        deviation = deviation/(len(image))**2
+        #51% because canvas background is mostly white
+        if deviation <= 0.31: #below 31% deviation means that they are quite similar
             return True #yes it is similar
     return False #if is not similar return False
 
