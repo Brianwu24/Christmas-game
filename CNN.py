f0d16616666dee822cb73b7c8be2a5347e7e4bd2
Brianwu24 <113204111+Brianwu24@users.noreply.github.com>
2022-12-05 21:42:45 -0500

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -13,8 +13,8 @@ class AI_Judges():
             self.judges[name] = load_model(self.model_files[model_number]) #load the model and append the name and model(tensorflow) to the dict
     def judge(self, model_key, input_image):
         input_image = np.array(input_image.copy(), np.int16).reshape(-1, 13, 13, 3) #reshape, and convert to np array for inferencing
-        output = self.judges[model_key].predict(input_image).squeeze() #squeeze() for faster processing
-        return output
+        output = self.judges[model_key].predict(input_image, verbose=0).squeeze() #squeeze() for faster processing
+        return round(float(output), 3)
 
 
 if __name__ == "__main__":
